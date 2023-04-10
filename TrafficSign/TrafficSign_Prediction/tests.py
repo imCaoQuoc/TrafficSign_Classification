198aee5023423c6aeaaafecb355d895f3a6dfa62
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-04-10 19:56:52 +0700

    delete all

--- TrafficSign/TrafficSign_Prediction/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-# Create your tests here.
-
-class SimpleTest(SimpleTestCase):
-    def test_traffice_page_status(self):
-        respone = self.client.get('/')
-        self.assertEquals(respone.status_code, 200)
