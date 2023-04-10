c954f957d2345ac210785ba76d67e4b846f49afd
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-04-10 18:36:17 +0700

    Update views.py

--- TrafficSign/TrafficSign_Prediction/views.py
+++ TrafficSign/TrafficSign_Prediction/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    response = HttpResponse()
+    response.writelines("<h1>Traffic Sign Prediction<h1>")
+    response.write("This is an application to predict traffic sign")
