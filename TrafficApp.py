496af713be7a5b06b2dc6b5fea7218e7e65de128
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-04-17 16:33:55 +0700

    Update TrafficApp.py

--- TrafficApp.py
+++ TrafficApp.py
@@ -12,6 +12,7 @@ st.image(image, caption='test')
 
 resize_image = image.resize([30, 30])
 array_image = np.array(resize_image)
+img_array = array_image.reshape((1,) + array_image.shape)
 
 predict = np.argmax(model.predict(resize_image), axis=-1)
 st.write(predict)
