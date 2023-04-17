9e775af9780122cad72ae33508ba1576e34e6f50
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-04-17 16:36:20 +0700

    Update TrafficApp.py

--- TrafficApp.py
+++ TrafficApp.py
@@ -4,7 +4,7 @@ import streamlit as st
 from tensorflow import keras
 from PIL import Image
 
-model = tf.keras.models.load_model("D:\predict_traffic_sign\model.h5", compile=False)
+model = tf.keras.models.load_model("D:\predict_traffic_sign\model.h5")
 
 img = st.file_uploader("Upload your image")
 image = Image.open(img)
@@ -12,7 +12,8 @@ st.image(image, caption='test')
 
 resize_image = image.resize([30, 30])
 array_image = np.array(resize_image)
-img_array = array_image.reshape((1,) + array_image.shape)
+array_image = array_image / 255.0
+#img_array = array_image.reshape((1,) + array_image.shape)
 
 predict = np.argmax(model.predict(resize_image), axis=-1)
 st.write(predict)
